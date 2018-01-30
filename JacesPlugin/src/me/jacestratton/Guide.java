6c671152b1814418be06cc0963335228b8228467
JaceStratton <immafirenmalaza@gmail.com>
2018-01-30 13:21:01 -0500

    First Upload: New plugin.

---
+++ JacesPlugin/src/me/jacestratton/Guide.java
@@ -0,0 +1,147 @@
+package me.jacestratton;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * Array of tips that can be added to and searched through.
+ * 
+ * @author JaceStratton
+ * 
+ * @version 1.0
+ */
+public class Guide {
+    private Scanner scanner;
+    private File file;
+    private ArrayList<String> tips;
+    private int numberOfTips;
+    private Random random;
+    private JacesPlugin plugin;
+
+    /**
+     * Default constructor that constructs an array of tips.
+     * 
+     * @throws FileNotFoundException
+     */
+    public Guide(JacesPlugin plugin) {
+        // Tries to create a new file from the list of tips. Logs errors.
+        try {
+            file = new File("tips.txt");
+            if (file.createNewFile()) {
+                FileWriter writer = new FileWriter(file, true);
+                writer.append("This is the first tip.");
+                writer.close();
+            }
+        }
+        catch (Exception e) {
+            plugin.printError(e);
+        }
+
+        // Tries to create a scanner from the file. Logs errors.
+        try {
+            scanner = new Scanner(file);
+        }
+        catch (Exception e) {
+            plugin.printError(e);
+        }
+
+        this.plugin = plugin;
+        tips = new ArrayList<String>();
+        numberOfTips = 0;
+        random = new Random();
+        while (scanner.hasNextLine()) {
+            tips.add(scanner.nextLine());
+            numberOfTips++;
+        }
+        scanner.close();
+    }
+
+    /**
+     * Prints a random tip to send to chat.
+     * 
+     * @return A random tip.
+     */
+    public String randomTip() {
+        int pos = random.nextInt(numberOfTips) + 1;
+        return printTip(pos);
+    }
+
+    /**
+     * Finds and prints the tip specified by the given number.
+     * 
+     * @param pos
+     *            The number of the tip's position in the array.
+     * @return The tip specified by the number.
+     */
+    public String searchTip(int pos) {
+        if (pos > 0 && pos <= numberOfTips) {
+            return printTip(pos);
+        }
+        return null;
+    }
+
+    /**
+     * Searches through the array of tips with a keyword.
+     * 
+     * @param keyword
+     *            The string to search tips for.
+     * @return The tip to return.
+     */
+    public String searchTip(String keyword) {
+        ArrayList<Integer> matches = new ArrayList<Integer>();
+        int j = 0;
+        for (int i = 0; i < numberOfTips; i++) {
+            if (tips.get(i).toLowerCase().contains(keyword.toLowerCase())) {
+                matches.add(i + 1);
+                j++;
+            }
+        }
+        if (j == 0) {
+            return null;
+        }
+        return printTip(matches.get(random.nextInt(j)));
+    }
+
+    /**
+     * Adds a tip to the tip array.
+     * 
+     * @param tip
+     *            The tip to add.
+     */
+    public void addTip(String tip) {
+        try {
+            FileWriter writer = new FileWriter(file, true);
+            writer.append("\n" + tip);
+            writer.close();
+            tips.add(tip);
+            numberOfTips++;
+        }
+        catch (IOException e) {
+            plugin.printError(e);
+        }
+    }
+
+    /**
+     * @return The number of tips.
+     */
+    public int getNumberOfTips() {
+        return numberOfTips;
+    }
+
+    /**
+     * Prints the specified tip.
+     * 
+     * @param pos
+     *            The position of the tip in the array.
+     * @return The tip specified by position.
+     */
+    private String printTip(int pos) {
+        return pos + "/" + numberOfTips + " - " + tips.get(pos - 1);
+    }
+
+}
---
+++ JacesPlugin/src/me/jacestratton/JacesPlugin.java
@@ -0,0 +1,307 @@
+package me.jacestratton;
+
+import java.util.logging.Logger;
+
+import org.bukkit.Server;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import net.md_5.bungee.api.ChatColor;
+
+/**
+ * The main class to initialize the plugin and run commands.
+ * 
+ * @author JaceStratton
+ * 
+ * @version 1.0
+ */
+public class JacesPlugin extends JavaPlugin {
+    private Guide tips;
+    private String message;
+    private Logger logger;
+    private Cooldown tipCooldown;
+    private Server server;
+    private Rules rules;
+
+    /**
+     * Initializes fields and logs the plugin initialization.
+     */
+    @Override
+    public void onEnable() {
+        server = getServer();
+        logger = getLogger();
+        logger.info("JacesPlugin is online!");
+        tips = new Guide(this);
+        rules = new Rules(this);
+        tipCooldown = new Cooldown(10);
+    }
+
+    /**
+     * Closes any open processes and stops the plugin.
+     */
+    @Override
+    public void onDisable() {
+
+    }
+
+    /**
+     * Catches commands.
+     * 
+     * @param sender
+     *            The player who sent the command.
+     * @param cmd
+     *            The command that was entered by the player.
+     * @param label
+     *            ???
+     * @param args
+     *            The arguments included for the command.
+     */
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            String command = cmd.getName();
+            if (command.equalsIgnoreCase("tip")) {
+                return tip(player, args);
+            }
+            if (command.equalsIgnoreCase("addtip")) {
+                return addtip(player, args);
+            }
+            if (command.equalsIgnoreCase("rules")) {
+                return rules(player, args);
+            }
+            if (command.equalsIgnoreCase("addrule")) {
+                return addRule(player, args);
+            }
+            if (command.equalsIgnoreCase("removerule")) {
+                return removeRule(player, args);
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Returns a tip to the player, either random or searched for.
+     * 
+     * @param player
+     *            The player who sent the command.
+     * @param args
+     *            The number or keyword used to search for a tip.
+     * @return True if the player sent a valid command. False if the player
+     *         included too many arguments.
+     */
+    public boolean tip(Player player, String[] args) {
+        if (args.length == 0) {
+            if (tipCooldown.hasCooldown(player)) {
+                player.sendMessage(ChatColor.RED + "Guide: " + tipCooldown.getCooldown(player));
+                return true;
+            }
+            message = tips.randomTip();
+            server.broadcastMessage("[Server] " + player.getName() + " got tip " + message);
+            return true;
+        }
+        else {
+            Exception exception = null;
+            int parse = 0;
+            try {
+                parse = Integer.parseInt(args[0]);
+            }
+            catch (Exception e) {
+                exception = e;
+            }
+
+            // If there wasn't a problem getting an integer from the string, the
+            // exception will be null and the player wants to search by number.
+            if (exception == null) {
+                message = tips.searchTip(parse);
+                if (message == null) {
+                    player.sendMessage(ChatColor.RED + "Specify number between 1 and " + tips.getNumberOfTips() + ".");
+                    return false;
+                }
+                if (tipCooldown.hasCooldown(player)) {
+                    player.sendMessage(ChatColor.RED + "Guide: " + tipCooldown.getCooldown(player));
+                    return true;
+                }
+                server.broadcastMessage("[Server] " + player.getName() + " searched for tip " + message);
+            }
+
+            // If an integer could not be retrieved from the string, the method
+            // will return an exception and the argument is a keyword.
+            else {
+                StringBuilder string = new StringBuilder(args[0]);
+                for (int i = 1; i < args.length; i++) {
+                    string.append(" " + args[i]);
+                }
+                message = tips.searchTip(string.toString());
+                if (message == null) {
+                    player.sendMessage(ChatColor.RED + "No matches found.");
+                    return false;
+                }
+                if (tipCooldown.hasCooldown(player)) {
+                    player.sendMessage(ChatColor.RED + "Tips: " + tipCooldown.getCooldown(player));
+                    return true;
+                }
+                server.broadcastMessage("[Server] " + player.getName() + " searched for tip " + message);
+            }
+            return true;
+        }
+    }
+
+    /**
+     * Adds a tip to the guidebook.
+     * 
+     * @param player
+     *            The player who wants to add a tip.
+     * @param args
+     *            The tip the player wants to add.
+     * @return True if the tip was not empty. False if the tip was empty.
+     */
+    public boolean addtip(Player player, String[] args) {
+        if (args.length != 0) {
+            if (tipCooldown.hasCooldown(player)) {
+                player.sendMessage(ChatColor.RED + "Guide: " + tipCooldown.getCooldown(player));
+                return true;
+            }
+            StringBuilder string = new StringBuilder(args[0]);
+            for (int i = 1; i < args.length; i++) {
+                string.append(" " + args[i]);
+            }
+            tips.addTip(string.toString());
+            message = tips.searchTip(tips.getNumberOfTips());
+            server.broadcastMessage("[Server] " + player.getName() + " added tip " + message);
+            return true;
+        }
+        player.sendMessage(ChatColor.RED + "Can't add an empty tip.");
+        return false;
+    }
+
+    /**
+     * Returns page of rules.
+     * 
+     * @param player
+     *            Player requesting rules.
+     * @param args
+     *            Rule page.
+     * @return True if page exists. False if not.
+     */
+    public boolean rules(Player player, String[] args) {
+        int pageNumber = 1;
+
+        if (args.length != 0) {
+            // Try to parse int from string.
+            try {
+                pageNumber = Integer.parseInt(args[0]);
+            }
+            catch (Exception e) {
+                printError(e);
+                player.sendMessage(ChatColor.RED + "Specify page number as digit.");
+                return false;
+            }
+        }
+
+        // If no exception, run second part.
+        int numberOfPages = rules.getRules().size() / 5;
+        if (rules.getRules().size() % 5 != 0) {
+            numberOfPages++;
+        }
+        if (pageNumber > numberOfPages || pageNumber <= 0) {
+            player.sendMessage(ChatColor.RED + "Specify number between 1 and " + numberOfPages);
+            return false;
+        }
+        else {
+            player.sendMessage(ChatColor.GOLD + "Rules(" + ChatColor.YELLOW + "Page " + pageNumber + "/" + numberOfPages
+                    + ChatColor.GOLD + ")");
+            int j = 0;
+            for (int i = (pageNumber * 5) - 5; i < rules.getRules().size(); i++) {
+                if (j == 5) {
+                    return true;
+                }
+                j++;
+                player.sendMessage(
+                        ChatColor.GOLD + Integer.toString(i + 1) + ChatColor.WHITE + " - " + rules.getRules().get(i));
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Adds rule to list.
+     * 
+     * @param player
+     *            Player adding rule.
+     * @param args
+     *            Rule to add.
+     * @return True if args has entry. False if not.
+     */
+    public boolean addRule(Player player, String[] args) {
+        if (args.length == 0) {
+            player.sendMessage(ChatColor.RED + "Must include rule.");
+            return false;
+        }
+        StringBuilder rule = new StringBuilder();
+        rule.append(args[0]);
+        for (int i = 1; i < args.length; i++) {
+            rule.append(" " + args[i]);
+        }
+        rules.addRule(rule.toString());
+        player.sendMessage(ChatColor.GREEN + "Rule added.");
+        return true;
+    }
+
+    /**
+     * Removes rule from list.
+     * 
+     * @param player
+     *            Player removing rule.
+     * @param args
+     *            Rule to remove.
+     * @return True if rule removed. False if not.
+     */
+    public boolean removeRule(Player player, String[] args) {
+        int ruleNumber = 0;
+
+        // Empty list will cause abnormalities.
+        if (rules.getRules().size() == 1) {
+            player.sendMessage(ChatColor.RED + "Can't remove only rule.");
+            return false;
+        }
+
+        if (args.length != 0) {
+            // Try to parse int from string.
+            try {
+                ruleNumber = Integer.parseInt(args[0]);
+            }
+            catch (Exception e) {
+                printError(e);
+                player.sendMessage(ChatColor.RED + "Specify number as digit.");
+                return false;
+            }
+        }
+        else {
+            player.sendMessage(ChatColor.RED + "Specify rule to remove.");
+            return false;
+        }
+
+        // If no exception, run second part.
+        if (ruleNumber <= 0 || ruleNumber > rules.getRules().size()) {
+            player.sendMessage(ChatColor.RED + "Specify number between 1 and " + rules.getRules().size() + ".");
+            return false;
+        }
+        rules.removeRule(ruleNumber);
+        player.sendMessage(ChatColor.GREEN + "Rule removed.");
+        return true;
+
+    }
+
+    /**
+     * Prints any error from GuideBook methods to the logger.
+     * 
+     * @param e
+     *            The error to print.
+     */
+    public void printError(Exception e) {
+        logger.info(e.getMessage());
+    }
+
+}
